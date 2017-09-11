4da715828cb433fb84d1a509a7aa3f6be68684fa
ReaganShort <15792597+ReaganShort@users.noreply.github.com>
2017-09-11 17:45:21 -0500

    Update DropTheBase.py

--- DropTheBase.py
+++ DropTheBase.py
@@ -7,6 +7,25 @@ def clearscreen():
         os.system('cls')
     else:
         os.system('clear')
+    print " _____               _______ _          ____                  "
+    print "|  __ \             |__   __| |        |  _ \                 "
+    print "| |  | |_ __ ___  _ __ | |  | |__   ___| |_) | __ _ ___  ___  "
+    print "| |  | | '__/ _ \| '_ \| |  | '_ \ / _ \  _ < / _` / __|/ _ \ "
+    print "| |__| | | | (_) | |_) | |  | | | |  __/ |_) | (_| \__ \  __/ "
+    print "|_____/|_|  \___/| .__/|_|  |_| |_|\___|____/ \__,_|___/\___| "
+    print "                 | |                                          "
+    print "                 |_|                                          "
+    print "-----------------------------------------"
+    print "/ This program recursively decodes base64 \\"
+    print "| text. If there are multiple layers of   |"
+    print "| encoding, each encoded instance will be |"
+    print "\ printed and numbered.                   /"
+    print "-----------------------------------------"
+    print "        \   ^__^"
+    print "         \  (oo)\_______"
+    print "            (__)\       )\/\\"
+    print "                ||----w |"
+    print "                ||     ||"
 
 def detect(encString):
     suspect = re.findall(base64regex, encString)
@@ -23,29 +42,11 @@ def decode(suspect):
                 print decString
                 print "\t\t\t\t\t*************************"
                 detect(decString)
-#            else:
-#                print "\n\t\t\t\t\tBase64 encoded instance " + str(count)
-#                print "\t\t\t\t\t*************************"
-#                print decString
+
 
 clearscreen()
-
-print " _____               _______ _          ____                  "
-print "|  __ \             |__   __| |        |  _ \                 "
-print "| |  | |_ __ ___  _ __ | |  | |__   ___| |_) | __ _ ___  ___  "
-print "| |  | | '__/ _ \| '_ \| |  | '_ \ / _ \  _ < / _` / __|/ _ \ "
-print "| |__| | | | (_) | |_) | |  | | | |  __/ |_) | (_| \__ \  __/ "
-print "|_____/|_|  \___/| .__/|_|  |_| |_|\___|____/ \__,_|___/\___| "
-print "                 | |                                          "
-print "                 |_|                                          "
-
-
-os.system('cowsay This program recursively decodes base64 text. If there are multiple layers of encoding, each encoded instance will be printed and numbered.')
-
 while True:
     count = 1
     base64regex = "(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})"
     encString = str(raw_input("\nPaste string to be decoded: "))
     detect(encString)
-
-
